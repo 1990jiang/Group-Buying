24624d5daf312b00cf600b570251054eb4cc5f42
Tmac <562752212@qq.com>
2016-12-22 16:35:47 +0800

    电商小项目的基本配置

---
+++ Group-Buying/Group-Buying/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Group-Buying
+//
+//  Created by 蒋俊 on 16/12/22.
+//  Copyright © 2016年 蒋俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Group-Buying/Group-Buying/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Group-Buying
+//
+//  Created by 蒋俊 on 16/12/22.
+//  Copyright © 2016年 蒋俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
